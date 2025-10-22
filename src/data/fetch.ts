c26d5687430b6d40f780397f2c195df5664d627e
Madina <madina.m.9509@gmail.com>
2025-10-22 10:38:39 +0500

    refactor: install ts and vite configs

---
+++ src/data/fetch.ts
@@ -0,0 +1,23 @@
+import { CoffeeItem } from '../types/product';
+
+const BASE_URL = 'https://6kt29kkeub.execute-api.eu-central-1.amazonaws.com/api';
+
+export async function fetchFavorites(): Promise<CoffeeItem[]> {
+  try {
+    const res = await fetch(`${BASE_URL}/favorites`);
+    if (!res.ok) throw new Error('Failed to fetch favorites');
+    return await res.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function fetchMenu(): Promise<CoffeeItem[]> {
+  try {
+    const res = await fetch(`${BASE_URL}/menu`);
+    if (!res.ok) throw new Error('Failed to fetch menu');
+    return await res.json();
+  } catch (error) {
+    throw error;
+  }
+}
