03664a2cd1166ccba30b29fdb1962b402de56c2c
Madina <madina.m.9509@gmail.com>
2025-11-06 22:02:52 +0500

    init: migrate html ts project into react

---
+++ my-app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/main.scss';
+
+const Home: React.FC = () => {
+  return (
+    <div className='home'>
+      <h1>Welcome to React + Vite</h1>
+      <p>This is migrated from plain HTML.</p>
+    </div>
+  );
+};
+
+export default Home;
