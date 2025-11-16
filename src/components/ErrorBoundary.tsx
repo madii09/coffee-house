647bd75d1b7a0542260e7ee74a479f4fb1f54fac
Madina <madina.m.9509@gmail.com>
2025-11-17 00:14:39 +0500

    feat: implement error boundary for runtime errors

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React, { type ReactNode } from 'react';
+import '../styles/main.scss';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error('ErrorBoundary caught an error', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <p>Please, refresh the page.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
