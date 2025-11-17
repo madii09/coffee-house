93a90ad3414c8a83ace2343f95596ffc50ed64bd
Madina <madina.m.9509@gmail.com>
2025-11-17 21:12:32 +0500

    feat: implement lint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,42 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist', 'node_modules']),
+
   {
     files: ['**/*.{ts,tsx}'],
+
     languageOptions: {
-      parser: '@typescript-eslint/parser',
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
         ecmaFeatures: { jsx: true },
       },
+      globals: {
+        ...globals.browser,
+      },
     },
+
     plugins: {
       '@typescript-eslint': tsPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
-    extends: [
-      js.configs.recommended,
-      tsPlugin.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+
     rules: {
       'no-console': 'warn',
-      'react/prop-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
+
     settings: {
       react: {
         version: 'detect',
